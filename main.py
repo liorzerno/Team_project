1dedfb31591fb8f171d080e4a03f1ae8d883fe83
noam <noam.m.inbar@gmail.com>
2026-09-08 16:10:45 +0300

    SCREEN!!

--- main.py
+++ main.py
@@ -3,22 +3,19 @@ import screen
 import pygame
 
 state = {
-        "original_bush": screen.create_bush(consts.GRASS_IMG),
+        "flag": screen.flag_display(consts.FLAG_IMG),
+        "soldier": screen.soldier_display(consts.SOLDIER_IMG),
+        "bush": screen.create_bush(consts.GRASS_IMG),
         "is_window_open": True,
         "state": consts.RUNNING_STATE
     }
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def main():
     pygame.init()
     while state["is_window_open"]:
         screen.draw_game(state)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- screen.py
+++ screen.py
@@ -8,14 +8,16 @@ pygame.init()
 
 screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 
-#background display
+#drawing the whole game
 def draw_game(game_state):
     screen.fill(consts.BACKGROUND_COLOR)
+    create_bush(game_state["bush"])
+    soldier_display(game_state["soldier"])
+    draw_message()
+    flag_display(game_state["flag"])
     pygame.display.flip()
-    create_bush(game_state["original_bush"])
 
-
-#image display
+#bush display randomly
 def create_bush(grass_img):
     grass_screen = pygame.image.load('grass.png')
     grass_screen = pygame.transform.scale(grass_screen, consts.GRASS_SIZE)
@@ -23,22 +25,46 @@ def create_bush(grass_img):
     grass_list=[]
 
     for i in range(consts.MINES_COUNT):
-        x_location = random.randint(0, consts.WINDOW_WIDTH)
-        y_location = random.randint(0, consts.WINDOW_HEIGHT)
+        x_location = random.randint(0, consts.WINDOW_WIDTH-30)
+        y_location = random.randint(0, consts.WINDOW_HEIGHT-30)
         grass_list.append((x_location,y_location))
-
 
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(consts.BACKGROUND_COLOR)
         for i in grass_list:
             screen.blit(grass_screen,i,grass_rect)
         pygame.display.flip()
 
-    #     x=random.randint(1,consts.WINDOW_WIDTH)
-    #     y=random.randint(1,consts.WINDOW_HEIGHT)
-    #     screen.blit(grass_screen,(x,y))
-    #     pygame.display.flip()
+#display of flag
+def flag_display(flag_img):
+    screen.fill(consts.BACKGROUND_COLOR)
+    flag_screen = pygame.image.load('flag.png')
+    flag_screen = pygame.transform.scale(flag_screen, consts.FLAG_SIZE)
+    screen.blit(flag_screen, (900,420,80,60))
+    pygame.display.flip()
+
+#display of soldier
+def soldier_display(soldier_img):
+    soldier_screen = pygame.image.load('soldier.png')
+    soldier_screen = pygame.transform.scale(soldier_screen, consts.SOLDIER_SIZE)
+    soldier_rect = soldier_screen.get_rect()
+    screen.blit(soldier_screen, soldier_rect)
+    pygame.display.flip()
+
+#welcome text
+def draw_message():
+    font = pygame.font.SysFont(consts.FONT_NAME, consts.WELCOME_FONT_SIZE)
+    text_img = font.render(consts.WELCOME_MESSAGE, True, consts.WHITE)
+    text_rect = text_img.get_rect()
+    screen.blit(text_img, text_rect)
+    pygame.display.flip()
+
+#enter mode
+# def enter_screen():
+
+
+
+
 
