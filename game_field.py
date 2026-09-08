233ef38c0bb48d829cbc3da659cc5c22f4c474f5
ktzhadas <hadask2008@gmail.com>
2026-09-08 10:09:07 +0300

    changed matrix type

--- game_field.py
+++ game_field.py
@@ -1,5 +1,7 @@
 import consts
 
 flag_row = consts.BOARD_ROWS - consts.FLAG_ROWS
-#box = {x_place: x, y_place: y, type: "bush"/"empty"}
+
+#box = {x_place: x, y_place: y, type: "mine"/ "flag" / "empty"}
 #game_field = [box, box...]
+
